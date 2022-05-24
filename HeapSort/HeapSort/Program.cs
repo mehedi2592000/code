5f5aabacb5af5df8eea17acb0d0da0902f28ecf5
mehedi <mehedi2592000@gmail.com>
2022-05-24 21:41:32 +0600

    heap sort

---
+++ HeapSort/HeapSort/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeapSort
+{
+    internal class Program
+    {
+        public static void sorte(int[] arr,int n)
+        {
+            for(int i=n/2-1;i>=0;i--)
+            {
+                heapyfy(arr,n,i);
+            }
+
+            for(int i=n-1;i>0;i--)
+            {
+                int temp=arr[0];
+                arr[0]=arr[i];
+                arr[i]=temp;
+
+                heapyfy(arr,i,0);
+            }
+        }
+
+        public static void heapyfy(int[] arr,int n,int i)
+        {
+            int largest=i;
+           int  l=2*i+1;
+            int r=2*i+2;
+
+            if(l<n && arr[l]>arr[largest])
+                largest=l;
+            if(r<n && arr[r]>arr[largest])
+                largest=r;
+
+            if(largest!=i)
+            {
+                int swape=arr[i];
+                arr[i]=arr[largest];
+                arr[largest]=swape;
+
+                heapyfy(arr,n,largest);
+            }
+
+              
+        }
+
+        public static void printe(int[] arr)
+        {
+            for(int i=0;i<arr.Length;i++)
+            {
+                Console.WriteLine(arr[i]);  
+            }
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 12, 11, 13, 4, 6, 7 };
+            int n = arr.Length;
+
+            sorte(arr, n);
+
+            printe(arr);
+        }
+    }
+}
---
+++ Sorting Patice All/Sorting Patice All/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_Patice_All
+{
+    internal class Program
+    {
+        public static void Sore(int[] arr,int n,int i)
+        {
+            int largest = i;
+            int l = 2 * i + 1;
+            int r = 2 * i + 2;
+            
+            if(l<n && arr[l]>arr[largest])
+                largest = l;
+            if(r<n && arr[r]>arr[largest])
+                largest=r;
+
+            if(largest!=i)
+            {
+                int tem=arr[i];
+                arr[i]=arr[largest];
+                arr[largest]=tem;
+
+                Sore(arr, n, largest);
+            }
+        }
+
+        public static void HeapSort(int[] arr,int n)
+        {
+            for(int i=n/2-1;i>=0;i--)
+            {
+                Sore(arr, n, i);
+            }
+
+            for(int i=n-1;i>0;i--)
+            {
+                int tem = arr[0];
+                arr[0]=arr[i];
+                arr[i] = tem;
+
+                Sore(arr, i,0);
+            }
+        }
+        public static void Print(int[] arr,int n)
+        {
+            for(int i=0;i<n;i++)
+            {
+                Console.WriteLine(arr[i] + " ");
+            }
+
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 8, 1, 16, 4, 20, 25 };
+            int n=arr.Length;
+            HeapSort(arr, n);
+            Print(arr, n);
+        }
+    }
+}
