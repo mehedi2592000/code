d148bad2f81d2bbac029888baf04f9d5f1de1f2f
mehedi <mehedi2592000@gmail.com>
2022-05-28 21:26:42 +0600

    hackerrank

---
+++ Fibonaki search/Fibonaki search/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonaki_search
+{
+    internal class Program
+    {
+
+        public static int min(int x,int y)
+        {
+            return (x<=y) ? x : y;
+        }
+        public static int fibonakiie(int[] arr,int x,int n)
+        {
+            int fib2 = 0;
+            int fib1 = 1;
+            int fib = fib1+fib2;
+
+            while(fib < n)
+            {
+                fib2 = fib1;
+                fib1 = fib;
+                fib = fib1+fib2;
+            }
+
+            int ofset = -1;
+
+            while(fib>1)
+            {
+                int i = min(ofset + fib2, n - 1);
+
+                if (arr[i] < x)
+                {
+                    fib = fib1;
+                    fib1 = fib2;
+                    fib2 = fib - fib1;
+                    ofset = i;
+                }
+                else if (arr[i] > x)
+                {
+                    fib = fib2;
+                    fib1 = fib1 - fib2;
+                    fib2 = fib - fib1;
+                }
+                else
+                    return i;
+                
+            }
+
+            if(fib1==1 && arr[n-1]==x)
+            {
+                return n - 1;
+            }
+
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 22, 35, 40, 45, 50, 80, 82, 85, 90, 100, 230 };
+
+            int n = arr.Length;
+
+            int r = fibonakiie(arr,230, n);
+
+            Console.WriteLine(r);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HackerRank1/HackerRank1/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank1
+{
+    internal class Program
+    {
+        public static void Sore(int[] arr,int n, int m)
+        {
+            int largest = m;
+            int Left = m * 2 + 1;
+            int Right = m * 2 + 2;
+
+            if(Left<n && arr[Left]>arr[largest])
+            {
+                largest = Left;
+            }
+
+            if(Right<n && arr[Right]>arr[largest])
+                largest = Right;
+
+            if(largest!=m)
+            {
+                int z=arr[largest];
+                arr[largest]=arr[m];
+                arr[m] = z;
+
+                Sore(arr,n,largest);
+            }
+        }
+
+        public static void Heap(int[] arr, int n)
+        {
+            int q = n / 2 - 1;
+
+            for(int i = q;i>=0; i--)
+            {
+                Sore(arr, n, i);
+            }
+
+            for(int i=n-1;i>0;i--)
+            {
+                int t = arr[0];
+                arr[0]=arr[i];
+                arr[i] = t;
+
+                Sore(arr, i, 0);
+            }
+        }
+        
+        public static int Binary(int[] arr,int i,int t,int x)
+        {
+            int r = i + (t - i) / 2;
+
+            if (arr[r] == x)
+                return r;
+
+            if (arr[r] > x)
+                return Binary(arr, 0, r - 1, x);
+
+            return Binary(arr, r + 1, t, x);
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                int k = Convert.ToInt32(Console.ReadLine());
+
+               int m = Convert.ToInt32(Console.ReadLine());
+                //int[] arr = { 4, 2, 3, 1, 5, 6 };
+                
+                int[] arr = new int[m];
+                
+                for(int j = 0; j < arr.Length; j++)
+                {
+                    arr[j] = Convert.ToInt32(Console.ReadLine());
+                }
+                
+                Heap(arr, arr.Length);
+                
+                for(int d=0;d<arr.Length; d++)
+                {
+                    int t=arr[d];
+                    int p = k - t;
+                    int h=Binary(arr, 0, arr.Length - 1, p);
+
+                    if(arr[h]+t==k)
+                    {
+                        Console.WriteLine(arr[h] + " " + t);
+                        break;
+                    }
+                }
+                
+                
+
+
+                
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
