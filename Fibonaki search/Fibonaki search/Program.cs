2e571c279a70c37929f9c844b3ae227980b6b5fd
mehedi <mehedi2592000@gmail.com>
2022-06-04 21:44:55 +0600

    Graph

--- Fibonaki search/Fibonaki search/Program.cs
+++ Fibonaki search/Fibonaki search/Program.cs
@@ -9,55 +9,48 @@ namespace Fibonaki_search
     internal class Program
     {
 
-        public static int min(int x,int y)
-        {
-            return (x<=y) ? x : y;
-        }
         public static int fibonakiie(int[] arr,int x,int n)
         {
             int fib2 = 0;
             int fib1 = 1;
-            int fib = fib1+fib2;
+            int fib=1;
 
-            while(fib < n)
+            while(fib<n)
             {
                 fib2 = fib1;
                 fib1 = fib;
-                fib = fib1+fib2;
+                fib=fib2+ fib1;
             }
 
             int ofset = -1;
 
             while(fib>1)
             {
-                int i = min(ofset + fib2, n - 1);
+                int i=Math.Min((ofset+fib2),(n-1));
 
                 if (arr[i] < x)
                 {
                     fib = fib1;
                     fib1 = fib2;
-                    fib2 = fib - fib1;
+                    fib2 = fib - fib2;
                     ofset = i;
                 }
                 else if (arr[i] > x)
                 {
-                    fib = fib2;
-                    fib1 = fib1 - fib2;
+                    fib = fib1;
+                    fib1 = fib - 1;
                     fib2 = fib - fib1;
+                    ofset = i;
                 }
                 else
                     return i;
-                
             }
-
-            if(fib1==1 && arr[n-1]==x)
-            {
+            if (fib1 == 1 && arr[n - 1] == x)
                 return n - 1;
-            }
 
             return -1;
-        }
 
+        }
         static void Main(string[] args)
         {
             int[] arr = { 10, 22, 35, 40, 45, 50, 80, 82, 85, 90, 100, 230 };
---
+++ Search_Algo/Search_Algo/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search_Algo
+{
+    internal class Program
+    {
+        public static void addEdge(LinkedList<int>[] adj,int u,int v)
+        {
+            adj[u].AddLast(v);
+            adj[v].AddLast(u);
+        }
+
+        public static void printgraph(LinkedList<int>[] adj)
+        {
+            for(int i = 0; i < adj.Length; i++)
+            {
+                Console.WriteLine("root"+adj[i]);
+
+                foreach(int v in adj[i])
+                {
+                    Console.WriteLine(v);
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+
+            int v = 5;
+            LinkedList<int>[] adj=new LinkedList<int>[v];
+
+            for(int i = 0; i < v; i++)
+            {
+                adj[i]=new LinkedList<int>();
+            }
+
+            addEdge(adj, 0, 1);
+            addEdge(adj, 0, 4);
+            addEdge(adj, 1, 2);
+            addEdge(adj, 1, 3);
+            addEdge(adj, 1, 4);
+            addEdge(adj, 2, 3);
+            addEdge(adj, 3, 4);
+
+            printgraph(adj);
+        }
+    }
+}
